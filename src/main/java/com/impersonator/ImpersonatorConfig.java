5242b67e5214c289cef4cd721b50142bfe0dd24b
no0bi1 <36062371+no0bi1@users.noreply.github.com>
2026-09-19 13:09:10 -0400

    Update ImpersonatorConfig.java

--- src/main/java/com/impersonator/ImpersonatorConfig.java
+++ src/main/java/com/impersonator/ImpersonatorConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("impersonator")
+@ConfigGroup("impersonatorplugin")
 public interface ImpersonatorConfig extends Config
 {
     @ConfigItem(
